1633093e9f9b161dd160c4acd2b77e5403b0e7f6
Oscards86 <93681478+Oscards86@users.noreply.github.com>
2022-06-23 15:03:38 -0600

    Create 2022_06_23_073642_create_pedidos_table.php

---
+++ database/migrations/2022_06_23_073642_create_pedidos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pedidos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre_Producto');
+            $table->string('marca_Modelo');
+            $table->string('dimension');
+            $table->dateTime('fecha_de orden');
+            $table->integer('existencia');
+            $table->string('colore_Producto');
+            $table->double('precio_Producto');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pedidos');
+    }
+};
