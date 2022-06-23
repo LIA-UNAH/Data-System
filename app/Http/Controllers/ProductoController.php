25b5050634c6cc091e62b02191b057951412e4c0
jenniferlazo-unah <88951754+jenniferlazo-unah@users.noreply.github.com>
2022-06-23 00:14:47 -0600

    Create ProductoController.php
    creacion del controlador de productos

---
+++ app/Http/Controllers/ProductoController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Producto;
+use Illuminate\Http\Request;
+
+class ProductoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $produc = Producto::all();
+        return view('producto.producto_inicio')->with('productos', $produc);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $crearprod = new Producto();
+
+        $crearprod->nombre = $request->input('nombre');
+        $crearprod->cantidad = $request->input('cantidad');
+        $crearprod->prec_compra = $request->input('prec_compra');
+        $crearprod->prec_venta = $request->input('prec_venta');
+        $crearprod->existencia = $request->input('existencia');
+        $crearprod->impuesto = $request->input('impuesto');
+        $crearprod->total = $request->input('total');
+
+        $crearprod->save();
+        
+        return redirect()->back();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Producto  $producto
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Producto $producto)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Producto  $producto
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Producto $producto)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Producto  $producto
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Producto $producto)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Producto  $producto
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Producto $producto)
+    {
+        //
+    }
+}
