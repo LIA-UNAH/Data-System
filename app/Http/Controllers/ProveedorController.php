cc4013d24fa36906c15aca7b19ee1bec21769252
jenniferlazo-unah <88951754+jenniferlazo-unah@users.noreply.github.com>
2022-06-27 23:22:59 -0600

    Update ProveedorController.php
    Modificaciones en los métodos del controlador de proveedores para editar, eliminar y buscar

--- app/Http/Controllers/ProveedorController.php
+++ app/Http/Controllers/ProveedorController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Proveedor;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ProveedorController extends Controller
 {
@@ -12,10 +13,16 @@ class ProveedorController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        $proveedor = Proveedor::all();
-        return view('proveedor.proveedores_index')->with('proveedor', $proveedor);
+        //$proveedor = Proveedor::all();
+        //SE AGREGO BUSCAR Y SE ESTA PAGINANDO DE 10 EN 10 
+        $buscar = trim( $request->get('buscar_texto'));
+        $proveedor=DB::table('proveedors')->select('id','nombre_proveedor',
+        'rtn_proveedor','telefono_proveedor','direccion_proveedor','contacto_proveedor','telefono_contacto_proveedor')
+        ->where('nombre_proveedor', 'like', '%'.$buscar.'%')
+        ->orWhere('rtn_proveedor', 'LIKE', '%'. $buscar. '%')->paginate(10);
+        return view('proveedor.proveedores_index', compact('proveedor', 'buscar'));// ESTAS 2 VARIABLES SON LAS QUE SE INICIALIZARON ARRIBA
     }
 
     /**
@@ -68,7 +75,8 @@ class ProveedorController extends Controller
      */
     public function edit($id)
     {
-        //
+        $proveedor=Proveedor::FindOrFail($id);
+        return view('proveedor.proveedores_index', compact('proveedors'));
     }
 
     /**
@@ -80,7 +88,16 @@ class ProveedorController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $proveedor=Proveedor::findOrfail($id);
+        $proveedor->nombre_proveedor =$request->input('nombre_proveedor');
+        $proveedor->rtn_proveedor = $request->input('rtn_proveedor');
+        $proveedor->telefono_proveedor = $request->input('telefono_proveedor');
+        $proveedor->direccion_proveedor = $request->input('direccion_proveedor');
+        $proveedor->contacto_proveedor = $request->input('contacto_proveedor');
+        $proveedor->telefono_contacto_proveedor = $request->input('telefono_contacto_proveedor');
+
+        $proveedor->save();
+        return redirect()->back();
     }
 
     /**
@@ -91,6 +108,9 @@ class ProveedorController extends Controller
      */
     public function destroy($id)
     {
-        //
+        Proveedor::destroy($id);
+        return redirect()->route('proveedor.index');
     }
+
+
 }
