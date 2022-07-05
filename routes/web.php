6a1746e65bad079e44a53cdbab9b1bf7095ca3f9
Oscards86 <93681478+Oscards86@users.noreply.github.com>
2022-07-05 11:11:09 -0600

    Ruta de edit y update de productos

--- routes/web.php
+++ routes/web.php
@@ -83,6 +83,15 @@ Route::delete('/clientes/{user}', [App\Http\Controllers\ClienteController::class
 
 //HU20 - #PRODUCTOS# index, create, store, show, edit, update, destroy
 Route::resource('/productos', ProductoController::class);
+// Editar Producto
+Route::get('/productos/{id}/editar',  [App\Http\Controllers\ProductoController::class, 'edit'])
+->name('producto.edit')
+->where('id','[0-9]+');
+
+// Actualizar Producto
+Route::put('/productos/{id}/editar', [App\Http\Controllers\ProductoController::class, 'update'])
+->name('producto.update')
+->where('id','[0-9]+');
 
 /*
 |--------------------------------------------------------------------------
