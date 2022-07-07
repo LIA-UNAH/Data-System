6c3e6c4226b97aa2df4977084b3d1e6ed33ad2a8
WilNher21 <wnhernandezf@unah.hn>
2022-07-07 14:17:51 -0600

    Update ventas_create

--- resources/views/venta/ventas_create.blade.php
+++ resources/views/venta/ventas_create.blade.php
@@ -1,158 +1,310 @@
 @extends('Layouts.Layouts')
 @section('content')
+<style>
+    body{
+    margin-top:20px;
+    color: #484b51;
+}
+.text-secondary-d1 {
+    color: #728299!important;
+}
+.page-header {
+    margin: 0 0 1rem;
+    padding-bottom: 1rem;
+    padding-top: .5rem;
+    border-bottom: 1px dotted #e2e2e2;
+    display: -ms-flexbox;
+    display: flex;
+    -ms-flex-pack: justify;
+    justify-content: space-between;
+    -ms-flex-align: center;
+    align-items: center;
+}
+.page-title {
+    padding: 0;
+    margin: 0;
+    font-size: 1.75rem;
+    font-weight: 300;
+}
+.brc-default-l1 {
+    border-color: #dce9f0!important;
+}
 
-<div>
+.ml-n1, .mx-n1 {
+    margin-left: -.25rem!important;
+}
+.mr-n1, .mx-n1 {
+    margin-right: -.25rem!important;
+}
+.mb-4, .my-4 {
+    margin-bottom: 1.5rem!important;
+}
 
-{{-- Mensajes de las operaciones realizadas --}}
-{{--Para los mensajes afirmativos y sin errores --}}
-@if (session()->has('suce'))
-    <div class="alert alert-success alert-dismissible" role="alert">
-        {{ session('suce') }}
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-    </div>
-@endif
-{{--Para los mensajes de errores --}}
-@if (session()->has('erorr'))
-    <div class="alert alert-danger" role="alert">
-        {{ session('erorr') }}
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-    </div>
-@endif
-{{-- Terminan los mensajes --}}
+hr {
+    margin-top: 1rem;
+    margin-bottom: 1rem;
+    border: 0;
+    border-top: 1px solid rgba(0,0,0,.1);
+}
 
-<div class="card shadow mb-4 ">
-        <div class="card-header py-3" style="background: #0d6efd">
-                <div style="float: left">
-                    <h2 class="m-0 font-weight-bold" style="color: white">Productos</h2>
-                </div>
+.text-grey-m2 {
+    color: #888a8d!important;
+}
 
-            <div style="float: right">
-                 <div style="float: left">
-                 <!-- HU8 - Buscar y recargar producto -->
-              
-                  <form action="" method="GET"
-                        class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-                      <div class="input-group">
-                          <input type="text" name="busqueda" class="form-control bg-light border-0 small"
-                                placeholder="Buscar"
-                                aria-label="Search" aria-describedby="basic-addon2">
-                            <div class="input-group-append">
-                              <button class="btn" type="submit" value="Buscar" style="background: white">
-                                  <i class="fas fa-search fa-sm" style="color: black"></i>
-                              </button>
-                          </div>
-                      </div>
-                  </form>
-                       <!-- HU8 - Buscar y recargar usuario -->
-            </div>
+.text-success-m2 {
+    color: #86bd68!important;
+}
+
+.font-bolder, .text-600 {
+    font-weight: 600!important;
+}
+
+.text-110 {
+    font-size: 110%!important;
+}
+.text-blue {
+    color: #478fcc!important;
+}
+.pb-25, .py-25 {
+    padding-bottom: .75rem!important;
+}
+
+.pt-25, .py-25 {
+    padding-top: .75rem!important;
+}
+.bgc-default-tp1 {
+    background-color: rgba(121,169,197,.92)!important;
+}
+.bgc-default-l4, .bgc-h-default-l4:hover {
+    background-color: #f3f8fa!important;
+}
+.page-header .page-tools {
+    -ms-flex-item-align: end;
+    align-self: flex-end;
+}
+
+.btn-light {
+    color: #757984;
+    background-color: #f5f6f9;
+    border-color: #dddfe4;
+}
+.w-2 {
+    width: 1rem;
+}
+
+.text-120 {
+    font-size: 120%!important;
+}
+.text-primary-m1 {
+    color: #4087d4!important;
+}
+
+.text-danger-m1 {
+    color: #dd4949!important;
+}
+.text-blue-m2 {
+    color: #68a3d5!important;
+}
+.text-150 {
+    font-size: 150%!important;
+}
+.text-60 {
+    font-size: 60%!important;
+}
+.text-grey-m1 {
+    color: #7b7d81!important;
+}
+.align-bottom {
+    vertical-align: bottom!important;
+}
+
+</style>
+
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+
+<div class="page-content container">
+    <div class="page-header text-blue-d2">
+        <h1 class="page-title text-secondary-d1">
+            Factura
+            <small class="page-info">
+                <i class="fa fa-angle-double-right text-80"></i>
+                ID: #111-222
+            </small>
+
+            <small id="dropdownMenuButton1"  data-bs-toggle="dropdown" aria-expanded="false" class="btn btn-primary dropdown-toggle">
+                Agregar Producto:
+            </small>
 
             
-              
+        </h1>
+
+        <div class="page-tools">
+            <div class="action-buttons">
+                <a class="btn bg-white btn-light mx-1px text-95" href="#" data-title="Print">
+                    <i class="mr-1 fa fa-print text-primary-m1 text-120 w-2"></i>
+                    Imprimir
+                </a>
+                <a class="btn bg-white btn-light mx-1px text-95" href="#" data-title="PDF">
+                    <i class="mr-1 fa fa-file-pdf-o text-danger-m1 text-120 w-2"></i>
+                    Guardar PDF
+                </a>
             </div>
         </div>
-
-    <!--------EMPIEZA LA TABLA ---------------->
-    <div class="card-body">
-        <div class="table-responsive">
-                  <table class="table table" id="dataTable" width="100%" >
-                      <thead class="card-header py-3" style="background: #1a202c; color:white">
-                        <tr>
-                          <th>N°</th>
-                          <th>Codigo</th>
-                          <th>Descripcion</th>
-                          <th>Precio</th>
-                          <th>Existencia</th>
-                          <th>Categoria</th>
-                          
-                          <th>Anadir</th>
-                        </tr>
-                  </thead>
-                  <tbody>
-                  @forelse($productos as $i=>  $product)
-                      <tr>
-                          <td scope="row">{{++$i}}</td>
-                          <td>{{ $product->codigo}}</td>
-                          <td scope="row">{{$product->descripcion}}</td>
-                          <td>{{ $product->prec_venta}}</td>
-                          <td>{{ $product->existencia}}</td>
-                          <td>{{ $product->categoria}}</td>
-                          <td><a class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop" href="">Anadir</a></td>
-                      </tr>
-                  @empty
-                      <tr>
-                          <td colspan="4">No hay productos ingresados</td>
-                      </tr>
-
-                  @endforelse
-              
-                  </tbody>
-
-          </table>
-         </div>
     </div>
 
-    <!-- Modal PARA CREAR UN NUEVO PRODUCTO -->
-         <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-                  <div class="modal-content">
-                    <div class="modal-header">
-                      <h5 class="modal-title" id="staticBackdropLabel">ANIADIR PRODUCTO</h5>
-                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    <div class="container px-0">
+        <div class="row mt-4">
+            <div class="col-12 col-lg-12">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="text-center text-150">
+                            
+                            <i class="fa fa-book fa-2x text-success-m2 mr-1"></i>
+                            <span class="text-default-d3">Data-System</span>
+                        </div>
+                    </div>
+                </div>
+              
+
+                <hr class="row brc-default-l1 mx-n1 mb-4" />
+
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div>
+                            <span class="text-sm text-grey-m2 align-middle">Nombre Cliente:</span>
+                            <span class="text-600 text-110 text-blue align-middle"></span>
+                        </div>
+                        <div class="text-grey-m2">
+                            <div class="my-1">
+                            <span class="text-sm text-grey-m2 align-middle">Direccion:</span>
+                            <span class="text-600 text-110 text-blue align-middle"></span>
+                            </div>
+                            <div class="my-1">
+                            <span class="text-sm text-grey-m2 align-middle">RTN Cliente:</span>
+                            <span class="text-600 text-110 text-blue align-middle"></span>
+                            </div>
+                            <div class="my-1">
+                            <span class="text-sm text-grey-m2 align-middle">Celular Cliente:</span>
+                            <span class="text-600 text-110 text-blue align-middle"></span>
+                            </div>
+
+                            </div>
+                    </div>
+                    <!-- /.col -->
+
+                    <div class="text-95 col-sm-6 align-self-start d-sm-flex justify-content-end">
+                        <hr class="d-sm-none" />
+                        <div class="text-grey-m2">
+                            <div class="mt-1 mb-2 text-secondary-m1 text-600 text-125">
+                                Datos Factura
+                            </div>
+
+                            <div class="my-2"><i class="fa fa-circle text-blue-m2 text-xs mr-1">
+                                </i> 
+                                <span class="text-600 text-90">ID:</span>
+                            </div>
+
+                            <div class="my-2" >
+                                <i class="fa fa-circle text-blue-m2 text-xs mr-1"  ></i> 
+                                <span class="text-600 text-90" >Fecha:
+                                    <span id="current_date">
+                                        <script>
+                                            date = new Date();
+                                            year = date.getFullYear();
+                                            month = date.getMonth() + 1;
+                                            day = date.getDate();
+                                            document.getElementById("current_date").innerHTML = day + "/" + month + "/" + year;
+                                        </script>
+                                    </span> 
+                                </span>
+                            </div>
+
+                            <div class="my-2">
+                                <i class="fa fa-circle text-blue-m2 text-xs mr-1"></i> 
+                                <span class="text-600 text-90">Vendido Por:{{ Auth::user()->name }}</span>   
+                                <span class="badge badge-warning badge-pill px-25"></span>
+                            </div>
+                        </div>
+                    </div>
+                   
+                </div>
+
+              
+                    
+
+                    <!-- or use a table instead -->
+                   
+            <div class="table-responsive">
+                <table class="table table-striped table-borderless border-0 border-b-2 brc-default-l1">
+                    <thead class="bg-none bgc-default-tp1">
+                        <tr class="text-white">
+                            <th class="opacity-2">Numero</th>
+                            <th>Descripcion</th>
+                            <th>Cantidad</th>
+                            <th>Precio Unitario</th>
+                            <th width="140">Total</th>
+                        </tr>
+                    </thead>
+
+                    <tbody class="text-95 text-secondary-d3">
+                        <tr></tr>
+                        <tr>
+
+                        </tr> 
+                    </tbody>
+                </table>
+            </div>
+           
+
+                    <div class="row mt-3">
+                        <div class="col-12 col-sm-7 text-grey-d2 text-95 mt-2 mt-lg-0">
+                            Reaizar Pago imediato de factura
+                        </div>
+
+                        <div class="col-12 col-sm-5 text-grey text-90 order-first order-sm-last">
+                            <div class="row my-2">
+                                <div class="col-7 text-right">
+                                    SubTotal
+                                </div>
+                                <div class="col-5">
+                                    <span class="text-120 text-secondary-d1"></span>
+                                </div>
+                            </div>
+
+                            <div class="row my-2">
+                                <div class="col-7 text-right">
+                                    Impuesto (15%)
+                                </div>
+                                <div class="col-5">
+                                    <span class="text-110 text-secondary-d1"></span>
+                                </div>
+                            </div>
+
+                            <div class="row my-2 align-items-center bgc-primary-l3 p-2">
+                                <div class="col-7 text-right">
+                                    Total 
+                                </div>
+                                <div class="col-5">
+                                    <span class="text-150 text-success-d3 opacity-2"></span>
+                                </div>
+                            </div>
+                        </div>
                     </div>
 
-                    <form action="{{ route('productos.store')}}" method="POST">
-                        @csrf
-                    <div class="modal-body">
-                  <div class="row g-3">
-                            
-                          </div>
-                          <div class="col-sm-6">
-                            <label for="firstName" class="form-label">Nombre producto:</label>
-                            <input type="text" class="form-control" id="nombre" name="nombre" placeholder="" value="{{ $product->descripcion}}" disabled>
-                            <div class="invalid-feedback">
-                              Valid first name is required.
-                            </div>
-                          </div>
+                    <hr />
 
-                          <div class="col-sm-6">
-                            <label for="firstName" class="form-label">Cantidad:</label>
-                            <input type="number" class="form-control" id="cantidad" name="cantidad" placeholder="Ingrese la cantidad" value="" required>
-                            <div class="invalid-feedback">
-                              Valid first name is required.
-                            </div>
-                          </div>
-                          <div class="col-sm-6">
-                            <label for="firstName" class="form-label">Precio Unitario:</label>
-                            <input type="number" class="form-control" id="prec_compra" name="prec_compra" placeholder="" value="{{ $product->prec_venta}}" disabled>
-                            <div class="invalid-feedback">
-                              Valid first name is required.
-                            </div>
-                          </div>
-                           <div class="col-sm-6">
-                            <label for="firstName" class="form-label">Total:</label>
-                            <input type="text" class="form-control" id="total" name="total" placeholder="Ingrese el total" value="" required>
-                            <div class="invalid-feedback">
-                              Valid first name is required.
-                            </div>
-                          </div> 
-                          
-                          </div>            
-                      </div>
+                    <div>
+                        <span class="text-secondary-d1 text-105">Gracias Por Comprar En Nuestro Negocio</span>
+                        
+                        <a href="#" class="btn btn-info btn-bold px-4 float-right mt-3 mt-lg-0">Guardar</a>
+                        <a href="/ventas" class="btn btn-danger btn-bold px-4 float-right mt-2 mt-lg-0" >Cancelar</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
-                      
-                      <!-----ESTE BOTON ES EL BOTON DEL MODAL PARA ANIADIR PRODUCTO A LA VENTA-->
-                      <div class="modal-footer">
-                          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                          <!-----<button type="submit" class="btn btn-primary">Guardar</button>-->
-                          
-                      </div>
-
-                      </form>
-                      </div>
-                  </div>
-                
-</div>
-
+  
 
     
     
