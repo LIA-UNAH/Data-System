9261e5d30bc0a1064d595b6a2753e08749832259
Oscards86 <93681478+Oscards86@users.noreply.github.com>
2022-07-25 01:12:36 -0600

    Creación de faker

--- database/factories/PedidoFactory.php
+++ database/factories/PedidoFactory.php
@@ -1,8 +1,8 @@
 <?php
 
 namespace Database\Factories;
-
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Pedido;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Pedido>
@@ -17,7 +17,13 @@ class PedidoFactory extends Factory
     public function definition()
     {
         return [
-            //
-        ];
+            'nombre_Cliente'=>$this->faker->name(),
+            'telefon_Cliente'=>$this->faker->e164PhoneNumber(),
+            'ciudad'=>$this->faker->city(),
+            'fecha_de_orden'=>$this->faker->date($format = 'Y-m-d', $max = 'now'),
+            'estado_Pedido'=>$this->faker->randomElement($array = array ('Entregado','No entregado')),
+            'detalle_Pedido'=>$this->faker->sentence(),
+            'total_Pedido'=>$this->faker->numberBetween($min = 10, $max = 40000)
+         ];
     }
 }
