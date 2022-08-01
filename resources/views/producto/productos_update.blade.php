f16f4ad80bd42ed16795937de367ead14a0d175d
Oscards86 <93681478+Oscards86@users.noreply.github.com>
2022-07-31 22:13:08 -0600

    Modificaciones en la plantilla de editar

--- resources/views/producto/productos_update.blade.php
+++ resources/views/producto/productos_update.blade.php
@@ -20,10 +20,29 @@
                 @method ('put')
                 <div class="modal-body">
                       <div class="row g-3">
+                     
+                      <div class="col-sm-6">
+                                <label for="firstName" class="form-label">Nombre:</label>
+                                <input type="text" class="form-control" id="nombre" name="nombre" 
+                                placeholder="Ingrese el nombre" value="{{$producto->nombre}}" required>
+                                @error('nombre')
+                                            <small class="text-danger" >
+                                                        <strong>{{ $message }}</strong>
+                                                    </small>
+                                            @enderror
+                                <div class="invalid-feedback">
+                                  Valid first name is required.
+                                </div>
+                              </div>
                               <div class="col-sm-6">
                                 <label for="firstName" class="form-label">Descripción:</label>
                                 <input type="text" class="form-control" id="descripcion" name="descripcion" 
                                 placeholder="Ingrese la descripcion" value="{{$producto->descripcion}}" required>
+                                @error('descripcion')
+                                    <small class="text-danger" >
+                                                <strong>{{ $message }}</strong>
+                                            </small>
+                                    @enderror
                                 <div class="invalid-feedback">
                                   Valid first name is required.
                                 </div>
@@ -33,7 +52,12 @@
                                 <label for="firstName" class="form-label">Código:</label>
                                 <input type="text" class="form-control" id="codigo" name="codigo"
                                  placeholder="Ingrese el código" value="{{$producto->codigo}}" required>
-                                <div class="invalid-feedback">
+                                 @error('codigo')
+                                    <small class="text-danger" >
+                                                <strong>{{ $message }}</strong>
+                                            </small>
+                                    @enderror
+                                 <div class="invalid-feedback">
                                   Valid first name is required.
                                 </div>
                               </div>
@@ -42,36 +66,69 @@
                                 <label for="firstName" class="form-label">Existencia:</label>
                                 <input type="number" class="form-control" id="existencia" name="existencia"
                                  placeholder="Ingrese la existencia" value="{{$producto->existencia}}" >
-                                <div class="invalid-feedback">
+                                 @error('existencia')
+                                    <small class="text-danger" >
+                                                <strong>{{ $message }}</strong>
+                                            </small>
+                                    @enderror
+                                 <div class="invalid-feedback">
                                   Valid first name is required.
                                 </div>
                               </div>
                               <div class="col-sm-6">
-                                <label for="firstName" class="form-label">Precio de venta:</label>
+                                <label for="firstName" class="form-label">Precio de compra:</label>
+                                <input type="number" class="form-control" id="prec_compra" name="prec_compra" 
+                                placeholder="Ingrese el precio de compra" value="{{$producto->prec_compra}}" required>
+                                @error('prec_compra')
+                                    <small class="text-danger" >
+                                                <strong>{{ $message }}</strong>
+                                            </small>
+                                    @enderror
+                                <div class="invalid-feedback">
+                                  Valid first name is required.
+                                </div>
+                              </div>
+                              
+                              <div class="col-sm-6">
+                                <label for="firstName" class="form-label">Precio de venta mayorista:</label>
                                 <input type="number" class="form-control" id="prec_venta" name="prec_venta" 
-                                placeholder="Ingrese el precio de venta" value="{{$producto->prec_venta}}" required>
+                                placeholder="Ingrese el precio de venta mayorista" value="{{$producto->prec_venta}}" required>
+                                @error('prec_venta')
+                                    <small class="text-danger" >
+                                                <strong>{{ $message }}</strong>
+                                            </small>
+                                    @enderror
                                 <div class="invalid-feedback">
                                   Valid first name is required.
                                 </div>
-                              </div>
-                              <div class="col-sm-6">
+                                </div>
+                                  <div class="col-sm-6">
                                 <label for="firstName" class="form-label">Categoría:</label>
                                 <input type="text" class="form-control" id="categoria" name="categoria" 
                                 placeholder="Ingrese la categoría" value="{{$producto->categoria}}" required>
+                                @error('categoria')
+                                    <small class="text-danger" >
+                                                <strong>{{ $message }}</strong>
+                                            </small>
+                                    @enderror
                                 <div class="invalid-feedback">
                                   Valid first name is required.
                                 </div>
                               </div> 
+
                               <div class="col-sm-6">
-                                <label for="firstName" class="form-label">Impuesto:</label>
-                                <input type="text" class="form-control" id="impuesto" name="impuesto"
-                                 placeholder="Ingrese el impuesto" value="{{$producto->impuesto}}" required>
-                                <div class="invalid-feedback">
-                                  Valid first name is required.
-                                </div>
-                              </div>
-                               
-                              
+                              <label class="form-label" for="customFile">Agregar Imagen:</label>
+                              <input type="file" class="form-control" id="imagen_producto" name="imagen_producto" 
+                              values="{{$producto->imagen_producto}}" required/>
+                              @error('imagen_producto')
+                                    <small class="text-danger" >
+                                                <strong>{{ $message }}</strong>
+                                            </small>
+                                    @enderror
+                               </div>
+
+                               <div class="col-sm-6" id="imagenSeleccionada" style="max-height: 300px;">
+                              </div>                   
                               </div>            
                           </div>
 
