9ff9c0334fe0949ecb80ab4336281b7476d820c9
Ana Romero <amromero@unah.hn>
2022-08-30 11:32:59 -0600

    Mejora visualizar clientes
    Se mejoró el apartado donde se visualizan los detalles generales de cada cliente.

--- resources/views/cliente/clientes_show.blade.php
+++ resources/views/cliente/clientes_show.blade.php
@@ -1,63 +1,55 @@
 @extends('Layouts.Layouts')
-@section('title', 'clientes')
+@section('title', 'Clientes')
 @section('content')
-
-    <div class="card shadow mb-4 ">
-        <div class="card-header py-3" style="background: #0d6efd">
-            <div style="float: left">
-                <h2 class="m-0 font-weight-bold" style="color: white">Visualizar {{$user->type}}</h2>
-            </div>
-        </div>
-
-        <div class="card-body">
-            <!-- Nested Row within Card Body -->
-            <div class="row" style="background: #4a5568">
-                <div class="col-lg-7" style="background: #1a202c; color: white; font-family: monospace">
+    <link href="/verproducto.css" rel="stylesheet" type="text/css"/>
+    <div class="container py-1">
+        <!-- Carta -->
+        <div class="card">
+            <div class=" text-center" style="font-size: 2em; background-color: #0c63e4; color: white"><strong>DETALLES DEL USUARIO</strong></div>
+            <div class="row ">
+                <div class="col-lg-8" style="background: whitesmoke; color: white; font-family: 'Nunito', sans-serif; font-size: small; text-align: justify">
                     <div class="p-5">
-                        <h2 style="text-align: center; background: #4a5568">{{$user->name}}</h2>
-                        <br>
-                        <h5 class="m-b-20 p-b-5 b-b-default f-w-600">Información principal</h5>
-                        <hr>
+                        <h5 class="m-b-20 p-b-5 b-b-default f-w-600" style="color: #1a202c; font-size: large"><strong>{{$user->name}}</strong></h5>
+                        <hr style="color: #1a202c; font-family: 'Nunito', sans-serif; font-size: large">
                         <div class="row">
-                            <div class="col-sm-6">
-                                <h6 class="m-b-10 f-w-600" style="color: #0d6efd">Correo electrónico:</h6>
-                                <h6  style="color: white; font-family:monospace;">{{$user->email}}</h6>
+                            <div class="col-sm-12" style="margin-top: 10px">
+                                <h6  style="color: #1a202c; font-size: large"><strong>Correo electrónico: </strong>{{$user->email}}</h6>
                             </div>
-                            <div class="col-sm-6">
-                                <h6 class="m-b-10 f-w-600" style="color: #0d6efd">Telefóno:</h6>
-                                <h6  style="color: white; font-family:monospace;">{{$user->telephone}}</h6>
-                            </div>
-                        </div>
 
-                        <br>
-                        <h5 class="m-b-20 m-t-40 p-b-5 b-b-default f-w-600">Información adicional</h5>
-                        <hr>
-                        <div class="row">
-                            <div class="col">
-                                <h6 class="m-b-10 f-w-600" style="color: #0d6efd">Dirección:</h6>
-                                <h6  style="color: white; font-family:monospace; text-align: justify">{{$user->address}}</h6>
+                            <div class="col-sm-12" style="margin-top: 10px">
+                                <h6  style="color: #1a202c; font-size: large"><strong>Telefóno: </strong>{{$user->telephone}}</h6>
+                            </div>
+
+                            <div class="col-sm-12"style="margin-top: 10px">
+                                <h6  style="color: #1a202c; font-size: large"><strong>Rol: </strong>{{$user->type}}</h6>
+                            </div>
+
+                            <div class="col-sm-12" style="margin-top: 10px">
+                                <h6  style="color: #1a202c; font-size: large"><strong>Dirección: </strong>{{$user->address}}</h6>
                             </div>
                         </div>
                     </div>
                 </div>
 
-                <div class="col-lg-5 d-none d-lg-block" style="text-align: center; padding-top: 50px">
-                    <img src="/images/uploads/{{ $user->image }}" width="300px" style="border-radius: 4%">
+                <div class="col-lg-4 text-center" style="padding-bottom: 10px">
+                    <div>
+                        <img src="/images/uploads/{{ $user->image }}" width="290px" height="290px"
+                             style="border-radius: 10%; padding: 15px">
+                    </div>
 
-                    <div class="col-sm-12 col-xs-12" style="text-align: center; margin-top: 15px; margin-bottom: 15px">
-                        <a href="{{route("clientes.edit",["id"=>$user->id])}}" style="margin-right: 4px; width: 145px; display: inline-block; background: #0f6848; color: white; border: 2px solid #ffffff;border-radius: 4px; font-size: large"
+                    <div class="text-center">
+                        <a href="{{route("clientes.edit",["id"=>$user->id])}}" style=" width: 130px; display: inline-block; background: #0d6efd; color: white; border: 2px solid #ffffff;border-radius: 10px; font-size: large"
                            class="btn btn-google btn-user ">
                             {{ __('Editar') }}
                         </a>
-                        <a href="/clientes" style="margin-left: 4px; width: 145px; display: inline-block; background: #b02a37; color: white; border: 2px solid #ffffff;border-radius: 4px; font-size: large"
+                        <a href="/clientes" style="width: 130px; display: inline-block; background: #b02a37; color: white; border: 2px solid #ffffff;border-radius: 10px; font-size: large"
                            class="btn btn-google btn-user ">
                             {{ __('Regresar') }}
                         </a>
                     </div>
                 </div>
-
-
             </div>
         </div>
+        <!-- Carta -->
     </div>
 @endsection
