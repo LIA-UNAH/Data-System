3ed7a2f544b9f9fffd0fdfc210561795583c6dd3
Oscards86 <osanchezg@unah.hn>
2022-09-26 22:19:12 -0600

    Controlador de la cuenta por cobrar

--- app/Http/Controllers/CuentasPorCobrarController.php
+++ app/Http/Controllers/CuentasPorCobrarController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\CuentasPorCobrar;
 
 class CuentasPorCobrarController extends Controller
 {
-    //
+    public function index()
+    {
+        $cobros = CuentasPorCobrar::paginate(10);
+        return view('cobros.cobros_index')->with('cobros', $cobros);
+    }
 }
