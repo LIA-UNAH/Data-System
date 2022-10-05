58888a32b1a26e9ad6a0b243cae1dbc105f43f4d
Nielsandr1998 <nvmeza@unah.hn>
2022-10-04 21:03:18 -0600

    Solución al error visual del menú

--- resources/views/layouts/layouts.blade.php
+++ resources/views/layouts/layouts.blade.php
@@ -301,7 +301,7 @@
         </li>
 
         <li class="nav-item" >
-            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseFourSix"
+            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsEleven"
                aria-expanded="true" aria-controls="collapseFourSix">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-wallet-fill" viewBox="0 0 16 16">
                     <path d="M1.5 2A1.5 1.5 0 0 0 0 3.5v2h6a.5.5 0 0 1 .5.5c0 .253.08.644.306.958.207.288.557.542 1.194.542.637 0 .987-.254 1.194-.542.226-.314.306-.705.306-.958a.5.5 0 0 1 .5-.5h6v-2A1.5 1.5 0 0 0 14.5 2h-13z"/>
@@ -309,7 +309,7 @@
                 </svg>
                 <span>Cobros</span>
             </a>
-            <div id="collapseFourSix" class="collapse" aria-labelledby="headingFourSix" data-parent="#accordionSidebar">
+            <div id="collapsEleven" class="collapse" aria-labelledby="headingFourSix" data-parent="#accordionSidebar">
                 <div class="bg-white py-2 collapse-inner rounded">
                     <a class="collapse-item" href="{{route('cobros.index')}}">Lista de cobros</a>
                 </div>
