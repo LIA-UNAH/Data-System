589f9bd5b3a95547e8147aa29a83f8780ef1ab2a
jenniferlazo-unah <88951754+jenniferlazo-unah@users.noreply.github.com>
2022-11-17 19:17:22 -0600

    Update VentaIndex.php

--- app/Http/Livewire/Ventas/VentaIndex.php
+++ app/Http/Livewire/Ventas/VentaIndex.php
@@ -16,25 +16,26 @@ class VentaIndex extends Component
         "estado" => [
             "nombre" => "Todas",
             "valor" => "",
-        ]
+        ],
     ];
+
+    public $sortField = 'fecha_factura';
+    public $sortDirection = 'desc';
 
     public function render()
     {
         return view('livewire..ventas.venta-index', [
-            'ventas' => Venta::where('estado', 'like', "%{$this->filtros["estado"]["valor"]}%")
-                ->where('numero_factura_venta', 'like', "%{$this->filtros["busqueda"]}%")
-                ->whereBetween('fecha_factura', [$this->filtros["fecha_inicial"], $this->filtros["fecha_final"]])
-                ->orderByDesc('fecha_factura')
-                ->paginate(10)
+                'ventas' => Venta::filter($this->filtros)
+                    ->orderBy($this->sortField, $this->sortDirection)
+                    ->paginate(10)
         ])
         ->extends('layouts.layouts')
         ->section('content');
     }
 
-    public function mount(){
-        $this->setFiltroFecha(Carbon::now()->toDateString('Y-m-d'), Carbon::now()->toDateString('Y-m-d'));
-    }
+    // public function mount(){
+    //     $this->setFiltroFecha(Carbon::now()->toDateString('Y-m-d'), Carbon::now()->toDateString('Y-m-d'));
+    // }
 
     public function setFiltroEstado($valor, $nombre){
         $this->filtros["estado"]["valor"] = $valor;
@@ -62,5 +63,15 @@ class VentaIndex extends Component
         }
 
         return redirect()->route('ventas.index')->with('success','¡Venta eliminada con éxito!');
+    }
+
+    public function sortBy($field){
+        if ($this->sortField === $field) {
+            $this->sortDirection = $this->sortDirection === 'asc' ? 'desc' : 'asc';
+        } else {
+            $this->sortDirection = 'asc';
+        }
+
+        $this->sortField = $field;
     }
 }
