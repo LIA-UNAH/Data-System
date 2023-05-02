5a3eafac867b8fed52916e73702ea39553629a36
Ana Romero <amromero@unah.hn>
2023-05-02 03:11:25 -0600

    Error 7 - Editar correo electrónico
    Tanto el usuario administrador, como el empleado y el cliente permiten actualizar el correo electrónico.

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -99,7 +99,7 @@ class RegisterController extends Controller
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
             'type' => $data['type'],
-            'customer' => 'Minorista',
+            'customer' => 'consumidor_final',
             'address' => $data['address'],
             'telephone' => $data['telephone'],
             'image' => 'Perfil (53).jpg',
