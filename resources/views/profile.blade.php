1c200972ccab37004b46eaa399e7489456a27a3c
Ana Romero <amromero@unah.hn>
2023-05-02 13:15:57 -0600

    Error 9 - Editar apellidos de usuario (N/A)
    No existe el apartado apellidos; solo el de nombre completo que se compone de nombres y apellidos.

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -75,24 +75,14 @@
                 <h4 class="card-title mt-2">{{$user->name}}</h4>
                 <h6 class="card-subtitle">{{$user->type}}</h6>
                 <br>
-                <div class="row text-center justify-content-md-center">
-                  <div class="col-4">
-                    <a class="rounded-pill" href="{{route("usuarios.edit_profile",["id"=>$user->id])}}" >
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
-                            <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/>
-                        </svg>
-                     </a>
+                  <div class="row justify-content-center">
+                      <div class="col-sm-6">
+                          <a href="{{ route('usuarios.edit_profile', ['id' => $user->id]) }}" class="btn btn-primary btn-block" id="btn-editar">Editar</a>
+                      </div>
+                      <div class="col-sm-6">
+                          <button type="button" class="btn btn-secondary btn-block" onclick="history.back()" id="btn-regresar">Regresar</button>
+                      </div>
                   </div>
-
-                  <div class="col-4">
-                    <a class="rounded-pill" href="javascript:history.back()" class="btn btn-primary">
-                           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-backspace" viewBox="0 0 16 16">
-                            <path d="M5.83 5.146a.5.5 0 0 0 0 .708L7.975 8l-2.147 2.146a.5.5 0 0 0 .707.708l2.147-2.147 2.146 2.147a.5.5 0 0 0 .707-.708L9.39 8l2.146-2.146a.5.5 0 0 0-.707-.708L8.683 7.293 6.536 5.146a.5.5 0 0 0-.707 0z"/>
-                            <path d="M13.683 1a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-7.08a2 2 0 0 1-1.519-.698L.241 8.65a1 1 0 0 1 0-1.302L5.084 1.7A2 2 0 0 1 6.603 1h7.08zm-7.08 1a1 1 0 0 0-.76.35L1 8l4.844 5.65a1 1 0 0 0 .759.35h7.08a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1h-7.08z"/>
-                          </svg>
-                        </a>
-                  </div>
-                </div>
               </center>
             </div>
           </div>
